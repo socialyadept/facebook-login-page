7460e7bbdc5d23932bc9b639915f4910bf60f3df
Muhammad Kumail <mkumailofficial@gmail.com>
2021-02-23 15:11:07 +0500

    Updates

---
+++ precache-manifest.880e7dda80c7cb65d585edc654aa3720.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd0887bf8279016108dc29299cbd8e7",
+    "url": "/facebook-login-page/index.html"
+  },
+  {
+    "revision": "a1799340b260012619fd",
+    "url": "/facebook-login-page/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "a143ffc56316c159ca66",
+    "url": "/facebook-login-page/static/css/main.fd99751e.chunk.css"
+  },
+  {
+    "revision": "a1799340b260012619fd",
+    "url": "/facebook-login-page/static/js/2.691c9f4e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/facebook-login-page/static/js/2.691c9f4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a143ffc56316c159ca66",
+    "url": "/facebook-login-page/static/js/main.0809734e.chunk.js"
+  },
+  {
+    "revision": "0a16310a8a22a08fd5e0",
+    "url": "/facebook-login-page/static/js/runtime-main.2186073f.js"
+  }
+]);
